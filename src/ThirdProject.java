7ebe798209f3b82b619e37207b2275f70300c813
Rashid <salehovr@gmail.com>
2021-10-05 17:55:40 -0400

    Group 10 third file

---
+++ src/ThirdProject.java
@@ -0,0 +1,9 @@
+public class ThirdProject {
+    public static void main(String[] args) {
+
+        System.out.println("Group 10 project 2 practice with third class");
+
+
+        // third
+    }
+}
