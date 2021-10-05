ea2003e629a7188d629067355fd03f329f8100d3
Rashid <salehovr@gmail.com>
2021-10-05 17:47:55 -0400

    First and second classes created for Group 10

---
+++ src/FirstProject.java
@@ -0,0 +1,8 @@
+public class FirstProject {
+
+    public static void main(String[] args) {
+
+        System.out.println("Group 10 project 2 practice");
+
+    }
+}
---
+++ src/SecondProject.java
@@ -0,0 +1,7 @@
+public class SecondProject {
+    public static void main(String[] args) {
+
+        System.out.println("Group 10 project 2 practice with second class");
+
+    }
+}
